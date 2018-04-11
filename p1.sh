ff52e31b19100888b1b5e68b9760ae7fdba31e10
Ramon Figueroa <ramondfd@hotmail.com>
2018-04-11 15:58:07 -0400

    Practica

---
+++ p1.sh
@@ -0,0 +1,8 @@
+
+#!/bin/bash
+
+echo "Usuario actual: $USER"
+echo "Directorio Home: $HOME"
+echo "Directorio actual: $PWD"
+echo "Los directorios que contiene el directorio actual:"
+echo "$(ls -1)"
